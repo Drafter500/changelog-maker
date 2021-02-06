7d543ba3503e884f75e312bed89bba12534b2330
drafter500 <vzemtsov@yahoo.com>
2021-02-06 10:41:34 +0100

    Adds function to get current repo owner and name

--- src/changelog-maker.js
+++ src/changelog-maker.js
@@ -6,6 +6,25 @@ function generateChangeLogText() {
   return 'test';
 }
 
+async function getCurrentRepoOwnerAndName() {
+  return new Promise((resolve, reject) => {
+    const getRepoUrlCommand = 'git config --get remote.origin.url';
+
+    require('child_process').exec(getRepoUrlCommand, function(err, stdout) {
+      if (err) {
+        reject(err);
+      }
+
+      let [ owner, repo ] = stdout.split('/').slice(-2);
+      if (repo.endsWith('\n')) {
+        repo = repo.slice(0, -1);
+      }
+
+      resolve([owner, repo]);
+    });
+  });
+}
+
 async function makeChangelog() {
 
 }
@@ -13,5 +32,6 @@ async function makeChangelog() {
 
 module.exports = {
   generateChangeLogText,
+  getCurrentRepoOwnerAndName,
   makeChangelog
 };
--- src/changelog-maker.test.js
+++ src/changelog-maker.test.js
@@ -1,9 +1,50 @@
 const { expect } = require('chai');
-const { generateChangeLogText } = require('./changelog-maker');
+const sinon = require('sinon');
+const mock = require('mock-require');
+const {
+  generateChangeLogText,
+  getCurrentRepoOwnerAndName,
+} = require('./changelog-maker');
 
 
-describe('generateChangeLogText', () => {
-  it('returns test', () => {
-    expect(generateChangeLogText()).to.equal('test');
+describe('changelog-maker', () => {
+  describe('generateChangeLogText', () => {
+    it('returns test', () => {
+      expect(generateChangeLogText()).to.equal('test');
+    });
+  });
+
+  describe('getCurrentRepoOwnerAndName', () => {
+    const execStub = sinon.stub();
+    mock('child_process',{exec: execStub});
+
+    beforeEach(() => {
+      execStub.reset();
+    });
+
+    it('rejects with error if command fails', async () => {
+      execStub.yields('error', null);
+
+      try {
+        await getCurrentRepoOwnerAndName();
+        expect.fail('should have failed');
+      } catch (e) {
+        expect(e).to.equal('error');
+      }
+    });
+
+    it('returns owner and repo if command succeeds', async () => {
+      execStub.yields(null, 'https://github.com/owner-one/repo-two');
+
+      const result = await getCurrentRepoOwnerAndName();
+      expect(result).to.deep.equal(['owner-one', 'repo-two']);
+    });
+
+    it('strips line feed from the repo name if present', async () => {
+      execStub.yields(null, 'https://github.com/owner-one/repo-two\n');
+
+      const result = await getCurrentRepoOwnerAndName();
+      expect(result).to.deep.equal(['owner-one', 'repo-two']);
+    });
   });
 });
